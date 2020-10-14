3d7d26b520b8a96ab5500000f26c7d894c8ca593
Think4dvantage <info@thinkadvantage.ch>
2020-10-14 05:35:37 +0200

    Added Explanation for Blending Command

--- VideoCombiner.ps1
+++ VideoCombiner.ps1
@@ -57,8 +57,29 @@ function CreateCamVideo($InputFolder)
     }
     #Output the Offset - for purposes
     write-host ("Video Offset is: " + $VideoOffset)
+
     #Prepare FFMPEG Arguments to Blend the two videos together
     $ffmpegarguments = ("-i $frontout -itsoffset 00:00:0$VideoOffset -i $backout -filter_complex " + [char]34 + "[1:v] scale=550:-1, pad=1920:1080:ow-iw-1360:oh-ih-10, setsar=sar=1, format=rgba [bs]; [0:v] setsar=sar=1, format=rgba [fb]; [fb][bs] blend=all_mode=addition:all_opacity=0.7" + [char]34 + " -vcodec libx265 -crf 28 $outputpath -hwaccel cuda -hwaccel_output_format cuda -y")
+    
+    #Example Argument: -i C:\Cyclevision\2020.10.01\Frontview.mp4 -itsoffset 00:00:01.889 -i C:\Cyclevision\2020.10.01\BackView.mp4 -filter_complex "[1:v] scale=550:-1, pad=1920:1080:ow-iw-1360:oh-ih-10, setsar=sar=1, format=rgba [bs]; [0:v] setsar=sar=1, format=rgba [fb]; [fb][bs] blend=all_mode=addition:all_opacity=0.7" -vcodec libx265 -crf 28 C:\Cyclevision\2020.10.01\.mp4 -hwaccel cuda -hwaccel_output_format cuda -y
+    #Explaining Arguments of Blending:
+    # -i C:\Cyclevision\2020.10.01\Frontview.mp4        -> first Input stored as [0]
+    # -itsoffset 00:00:01.889                           -> Offsetting the Start of Stream [1] by 01.889 Seconds (since the Videos dont Start at the same time)
+    # -i C:\Cyclevision\2020.10.01\BackView.mp4         -> second Input stored as [1]
+    # -filter_complex                                   -> start complex filter which enables you do to crazy things
+    # [1:v]                                             -> Take Videostream of Video 1
+    # scale=550:-1,                                     -> Scale Video [1] it to 550*302 Pixels
+    # pad=1920:1080:                                    -> Keep the size of the Stream of Video [1] on FullHD - this is needed so that the two steams can be blended together (blending needs same Resolution)
+    # ow-iw-1360:oh-ih-10                               -> put the Scaled video on the Bottomleft Corner with 10px padding (1360 + 550 = 1910 - makes a 10px margin) 
+    # setsar=sar=1, format=rgba [bs]                    -> set Color preset for blending and store the altered Stream of [1] as [bs]
+    # [0:v] setsar=sar=1, format=rgba [fb]              -> Set Color Preset for Video [0] and store it as [fb]
+    # [fb][bs] blend=all_mode=addition:all_opacity=0.7  -> Blend in [bs] on top of [fb] with an opacity of 0.7 = 70%
+    # -vcodec libx265 -crf 28                           -> Re-Encode Video with x265 Codec with a CRF (quality) setting of 28 - the lower the CRF the higher the Quality
+    # C:\Cyclevision\2020.10.01\.mp4                    -> Output Path for Operation -> this is the result of the Operation
+    # -hwaccel cuda -hwaccel_output_format cuda         -> enable ffmpeg to use Nvidia power to increase Speed of operation
+    # -y                                               -> Overwrite Output File if it already exists
+    # Why use blending instead of Overlay? to be able to use Opacity
+    
     #Blend FrontView and Backview together
     start-process -FilePath $ffmpegPath -ArgumentList $ffmpegarguments -PassThru -wait -nonewWindow
 }
